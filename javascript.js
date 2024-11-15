421460376fa3d0e1b564574ec2bc118f692aedf7
nidhiambatkar026 <nidhiambatkar@gmail.com>
2024-11-15 21:19:59 +0530

    javascript file separted

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function summer(){
+    const  image_screen = document.getElementById('season_image');
+    image_screen.src = './Images/summer.avif';
+ }
+ function winter(){
+    const image_screen = document.getElementById('season_image');
+    image_screen.src = './Images/Winter_season.jpg';
+ }
+ function Rainy(){
+    const image_screen= document.getElementById('season_image');
+    image_screen.src = './Images/rainy_season.avif';
+ }
